0347b880cd5109edd0fb8964488fdcfe95ec7e2d
Marco Alemanni <c.alemanni@elis.org>
2025-05-29 14:59:54 +0200

    implementation Factory method

---
+++ src/GuroDemo/VipCustomerFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuroDemo
+{
+    internal class VipCustomerFactory : ICustomerFactory
+    {
+        public Customer CreateCustomer(string name, string email)
+        {
+            Customer customer = new CustomerBuilder()
+                .WithName(name)
+                .WithEmail(email)
+                .WithType(TypeCustomerEnum.Vip)
+                .Build();
+            return customer;
+        }
+    }
+}
