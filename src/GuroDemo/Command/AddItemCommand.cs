9bd14a5efaa9e6b9cdbbd35716ea36890689e226
Marco Alemanni <c.alemanni@elis.org>
2025-06-09 12:17:47 +0200

    feat(order): add command pattern and implement OrderManager
    Introduced the command pattern to encapsulate order operations.
    Created an OrderManager to coordinate and execute commands,
    providing better control over the order flow.

---
+++ src/GuroDemo/Command/AddItemCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuroDemo.Command
+{
+    internal class AddItemCommand : ICommand
+    {
+        private readonly Order _order;
+        private readonly OrderItem _item;
+
+        public AddItemCommand(Order order, OrderItem item)
+        {
+            _order = order;
+            _item = item;
+        }
+
+        public void Execute()
+        {
+            _order.AddItem(_item.Product, _item.Quantity);
+        }
+
+        public void Undo()
+        {
+            _order.RemoveProduct(_item);
+        }
+    }
+}
+
---
+++ src/GuroDemo/Services/OrderManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GuroDemo.Decoration;
+
+namespace GuroDemo.Services
+{
+    internal class OrderManager
+    {
+        private readonly OrderSender _sender = OrderSender.GetInstance();
+
+        public void Run(Order order, ProductSelector selector)
+        {
+            _sender.RegisterObserver(new OrderPrinter(order));
+            _sender.RegisterObserver(new EmailNotifier());
+
+            bool continua = true;
+
+            while (continua)
+            {
+                OrderItem item = CreateOrderItem(selector);
+                order.AddItem(item.Product, item.Quantity);
+
+                PrintTotal(order);
+                continua = _sender.ContinueShopping();
+            }
+
+            Console.WriteLine("GoodBye " + order.Customer.Name);
+        }
+
+        private OrderItem CreateOrderItem(ProductSelector selector)
+        {
+            selector.ShowAvailableProducts();
+            Product product = selector.GetProduct();
+            int quantity = selector.GetProductQuantity(product);
+            product = ProductDecoratorService.AddDecorator(product);
+
+            return new OrderItem(product, quantity);
+        }
+
+        private void PrintTotal(Order order)
+        {
+            double total = order.Items.Sum((Func<OrderItem, double>)(item => (item.Product.GetPrice() * item.Quantity)));
+            IPriceStrategy strategy = PriceCalculator.GetStrategy(order.Customer.TypeCustomer);
+            double finalTotal = strategy.Calculate(total);
+
+            _sender.Send(order);
+            Console.WriteLine($"Order total: {finalTotal:F2} EUR");
+        }
+    }
+}
