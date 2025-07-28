1e35e4f16bc905e67074d9ced946fd8ea3775463
Marco Alemanni <c.alemanni@elis.org>
2025-07-28 11:18:51 +0200

    test(customer): add unit tests for CustomerBuilder
    Added unit tests to verify the behavior and correctness of
    the CustomerBuilder logic. Ensures consistent object creation
    and improves test coverage.

---
+++ src/Primo_Shopping.Tests/Builder/CustomerBuilderTest.cs
@@ -0,0 +1,73 @@
+﻿using GuroDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primo_Shopping.Tests.Builder
+{
+    public class CustomerBuilderTest
+    {
+        [Fact]
+        public void CustomerBuilder_ShouldCreateCustomer_WhenNameIsValid()
+        {
+            var customer = new CustomerBuilder()
+                .WithName("Mario")
+                .WithEmail("mario@email.com")
+                .WithType(CustomerTypeEnum.Private)
+                .Build();
+
+            Assert.Equal("Mario", customer.Name);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData(null)]
+        public void CustomerBuilder_ShouldThrowWhenNameIsNullOrEmpty(string? name)
+        {
+            var builder = new CustomerBuilder();
+
+            Assert.Throws<ArgumentException>(() =>
+                builder.WithName(name)
+                       .WithEmail("test@email.com")
+                       .WithType(CustomerTypeEnum.Private)
+                       .Build()
+            );
+        }
+
+        [Theory]
+        [InlineData("A")]
+        [InlineData("X")]
+        public void CustomerBuilder_ShouldThrowWhenNameIsTooShort(string name)
+        {
+            var builder = new CustomerBuilder();
+
+            Assert.Throws<ArgumentException>(() =>
+                builder.WithName(name)
+                       .WithEmail("test@email.com")
+                       .WithType(CustomerTypeEnum.Private)
+                       .Build()
+            );
+        }
+
+        [Theory]
+        [InlineData("Mari@")]
+        [InlineData("Jo#hn")]
+        [InlineData("Luc@123")]
+        [InlineData("Al€#ssio")]
+        public void CustomerBuilder_ShouldThrow_WhenNameContainsSpecialCharacters(string name)
+        {
+            var builder = new CustomerBuilder();
+
+            Assert.Throws<ArgumentException>(() =>
+                builder.WithName(name)
+                       .WithEmail("test@email.com")
+                       .WithType(CustomerTypeEnum.Private)
+                       .Build()
+            );
+        }
+
+    }
+}
